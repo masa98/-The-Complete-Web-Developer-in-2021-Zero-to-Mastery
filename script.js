52622f967d5ad979a92bcbb42ed23593ecb3564c
Masa Ajaj <masa.sam.98@gmail.com>
2021-09-13 15:54:30 +0300

    without 5

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var _ = require('lodash');
 
 var array=[1,2,3,4,5,6,7,8];
-console.log('answer:', _.without(array,3));
+console.log('answer:', _.without(array,5));
 console.log("hi",_);
-console.log("some js code");
+
 console.log("welcome there!");
